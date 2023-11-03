3baf1ab4fab526375508849f9947b427afc21c5a
Carolin <carolin.svensson@live.se>
2023-11-03 10:47:35 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,69 @@
-!function(e){var r={},t={0:0};function n(t){if(r[t])return r[t].exports;var o=r[t]={i:t,l:!1,exports:{}};return e[t].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.e=function(r){if(0!==t[r]){var n=require("./"+({}[r]||r)+".js"),o=n.modules,u=n.ids;for(var i in o)e[i]=o[i];for(var f=0;f<u.length;f++)t[u[f]]=0}return Promise.all([])},n.m=e,n.c=r,n.d=function(e,r,t){n.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,r){if(1&r&&(e=n(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var t=Object.create(null);if(n.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var o in e)n.d(t,o,function(r){return e[r]}.bind(null,o));return t},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},n.p="",n.oe=function(e){process.nextTick((function(){throw e}))},n(n.s=0)}([function(e,r,t){"use strict";Promise.all([t.e(1),t.e(2)]).then(t.bind(null,14))},function(e,r){e.exports=require("path")},function(e,r){e.exports=require("buffer")},function(e,r){e.exports=require("fs")},function(e,r){e.exports=require("http")},function(e,r){e.exports=require("stream")},function(e,r){e.exports=require("util")},function(e,r){e.exports=require("net")},function(e,r){e.exports=require("events")},function(e,r){e.exports=require("zlib")},function(e,r){e.exports=require("async_hooks")},function(e,r){e.exports=require("querystring")},function(e,r){e.exports=require("url")},function(e,r){e.exports=require("crypto")},,function(e,r){e.exports=require("tty")},function(e,r){e.exports=require("string_decoder")}]);
+!function(e){var r={},n={index:0},t={};var o={"./pkg/index_bg.wasm":function(){return{"./index_bg.js":{__wbindgen_throw:function(e,n){return r["./pkg/index_bg.js"].exports.__wbindgen_throw(e,n)}}}}};function u(n){if(r[n])return r[n].exports;var t=r[n]={i:n,l:!1,exports:{}};return e[n].call(t.exports,t,t.exports,u),t.l=!0,t.exports}u.e=function(r){var i=[];if(0!==n[r]){var s=require("./"+({}[r]||r)+".js"),a=s.modules,l=s.ids;for(var c in a)e[c]=a[c];for(var p=0;p<l.length;p++)n[l[p]]=0}return({1:["./pkg/index_bg.wasm"]}[r]||[]).forEach((function(e){var r=t[e];if(r)i.push(r);else{var n,s=o[e](),a=new Promise((function(r,n){var{readFile:t}=require("fs"),{join:o}=require("path");try{t(o(__dirname,{"./pkg/index_bg.wasm":"f32282d837b5f2d8f2ce"}[e]+".module.wasm"),(function(e,t){if(e)return n(e);r({arrayBuffer:()=>Promise.resolve(t)})}))}catch(e){n(e)}}));if(s instanceof Promise){var l=a.then((function(e){return e.arrayBuffer()}));n=Promise.all([l.then((function(e){return WebAssembly.compile(e)})),s]).then((function(e){return WebAssembly.instantiate(e[0],e[1])}))}else{n=(l=a.then((function(e){return e.arrayBuffer()}))).then((function(e){return WebAssembly.instantiate(e,s)}))}i.push(t[e]=n.then((function(r){return u.w[e]=(r.instance||r).exports})))}})),Promise.all(i)},u.m=e,u.c=r,u.d=function(e,r,n){u.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:n})},u.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},u.t=function(e,r){if(1&r&&(e=u(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(u.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var t in e)u.d(n,t,function(r){return e[r]}.bind(null,t));return n},u.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return u.d(r,"a",r),r},u.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},u.p="",u.oe=function(e){process.nextTick((function(){throw e}))},u.w={},u(u.s="./js/app_index.ts")}({"./js/app_index.ts":
+/*!*************************!*\
+  !*** ./js/app_index.ts ***!
+  \*************************/
+/*! no static exports found */function(module,exports,__webpack_require__){"use strict";eval('\nPromise.all(/*! import() */[__webpack_require__.e(0), __webpack_require__.e(1)]).then(__webpack_require__.bind(null, /*! ./app_main */ "./js/app_main.ts"));\n\n\n//# sourceURL=webpack:///./js/app_index.ts?')},async_hooks:
+/*!******************************!*\
+  !*** external "async_hooks" ***!
+  \******************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("async_hooks");\n\n//# sourceURL=webpack:///external_%22async_hooks%22?')},buffer:
+/*!*************************!*\
+  !*** external "buffer" ***!
+  \*************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("buffer");\n\n//# sourceURL=webpack:///external_%22buffer%22?')},crypto:
+/*!*************************!*\
+  !*** external "crypto" ***!
+  \*************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("crypto");\n\n//# sourceURL=webpack:///external_%22crypto%22?')},events:
+/*!*************************!*\
+  !*** external "events" ***!
+  \*************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("events");\n\n//# sourceURL=webpack:///external_%22events%22?')},fs:
+/*!*********************!*\
+  !*** external "fs" ***!
+  \*********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("fs");\n\n//# sourceURL=webpack:///external_%22fs%22?')},http:
+/*!***********************!*\
+  !*** external "http" ***!
+  \***********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("http");\n\n//# sourceURL=webpack:///external_%22http%22?')},net:
+/*!**********************!*\
+  !*** external "net" ***!
+  \**********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("net");\n\n//# sourceURL=webpack:///external_%22net%22?')},os:
+/*!*********************!*\
+  !*** external "os" ***!
+  \*********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("os");\n\n//# sourceURL=webpack:///external_%22os%22?')},path:
+/*!***********************!*\
+  !*** external "path" ***!
+  \***********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("path");\n\n//# sourceURL=webpack:///external_%22path%22?')},querystring:
+/*!******************************!*\
+  !*** external "querystring" ***!
+  \******************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("querystring");\n\n//# sourceURL=webpack:///external_%22querystring%22?')},stream:
+/*!*************************!*\
+  !*** external "stream" ***!
+  \*************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("stream");\n\n//# sourceURL=webpack:///external_%22stream%22?')},string_decoder:
+/*!*********************************!*\
+  !*** external "string_decoder" ***!
+  \*********************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("string_decoder");\n\n//# sourceURL=webpack:///external_%22string_decoder%22?')},tty:
+/*!**********************!*\
+  !*** external "tty" ***!
+  \**********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("tty");\n\n//# sourceURL=webpack:///external_%22tty%22?')},url:
+/*!**********************!*\
+  !*** external "url" ***!
+  \**********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("url");\n\n//# sourceURL=webpack:///external_%22url%22?')},util:
+/*!***********************!*\
+  !*** external "util" ***!
+  \***********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("util");\n\n//# sourceURL=webpack:///external_%22util%22?')},zlib:
+/*!***********************!*\
+  !*** external "zlib" ***!
+  \***********************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("zlib");\n\n//# sourceURL=webpack:///external_%22zlib%22?')}});
